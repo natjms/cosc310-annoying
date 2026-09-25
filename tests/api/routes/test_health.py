e5039c647c0049c651aa7ea2bdf10d44587170eb
Lua MacDougall <lua@foxgirl.dev>
2026-09-25 11:01:02 -0700

    Add `test_get_health_returns_empty_object`

--- tests/api/routes/test_health.py
+++ tests/api/routes/test_health.py
@@ -6,3 +6,7 @@ client = TestClient(app)
 def test_get_health_is_200():
 	response = client.get('/api/health')
 	assert response.status_code == 200
+
+def test_get_health_returns_empty_object():
+	response = client.get('/api/health')
+	assert response.json() == {}
